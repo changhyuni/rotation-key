cfdce5259782f590da8a44f5dc6d66d69fda89f4
changhyuni <bhs9610@naver.com>
2021-05-05 15:30:41 +0900

    test1

--- rotate.py
+++ rotate.py
@@ -11,25 +11,27 @@ resource = boto3.resource('iam')
 client = boto3.client("iam")
 
 KEY = 'LastUsedDate'
+OldkeyList = []
 
-for user in resource.users.all():
-    Metadata = client.list_access_keys(UserName=user.user_name)
-    if Metadata['AccessKeyMetadata']:
-        for key in user.access_keys.all():
-            
-            AccessId = key.access_key_id
-            Status = key.status
-            CreatedDate = key.create_date
+def get_old_access_key():
+    for user in resource.users.all():
+        Metadata = client.list_access_keys(UserName=user.user_name)
+        if Metadata['AccessKeyMetadata']:
+            for key in user.access_keys.all():
+                
+                # AccessId = key.access_key_id
+                Status = key.status
+                CreatedDate = key.create_date
 
-            numOfDays = diff_dates(utc_to_local(datetime.utcnow()), utc_to_local(CreatedDate))
-            LastUsed = client.get_access_key_last_used(AccessKeyId=AccessId)
+                numOfDays = diff_dates(utc_to_local(datetime.utcnow()), utc_to_local(CreatedDate))
+                # LastUsed = client.get_access_key_last_used(AccessKeyId=AccessId)
+                if numOfDays <= 90:
+                    OldkeyList.append(user.user_name)
+                # print(user.user_name, numOfDays)
 
-            if (Status == "Active"):
-                if KEY in LastUsed['AccessKeyLastUsed']:
-                    print("User:", user.user_name,  "Key:", AccessId, "Last Used:", LastUsed['AccessKeyLastUsed'][KEY], "Age of Key:", numOfDays, "Days")
-                else:
-                    print("User:", user.user_name , "Key:",  AccessId, "Key is Active but NEVER USED")
-            else:
-                print("User:", user.user_name , "Key:",  AccessId, "Keys is InActive")
-    else:
-        print("User:", user.user_name , "No KEYS for this USER")
+                # if (Status == "Active"):
+                #     if KEY in LastUsed['AccessKeyLastUsed']:
+                #         print("User:", user.user_name, numOfDays)
+
+
+print(OldkeyList)
